a7a0e243ccd6d01ef1e0af8d320909be3e80bbdc
EnzoMaliLip <enzo.lipnharski@escola.pr.gov.br>
2025-02-17 08:20:57 -0300

    Add files via upload

---
+++ ATV5/troca_de_valores.js
@@ -0,0 +1,14 @@
+let a = "Arroz";
+let b = "Feijão";
+
+console.log("a:", a);
+console.log("b:", b);
+
+let temp = a; 
+a = b;       
+b = temp;   
+
+console.log("a:", a);
+console.log("b:", b);
+
+
