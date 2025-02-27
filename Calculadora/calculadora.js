84ce70e21123e2dbf11facd363326bbbde57760b
EnzoMaliLip <enzo.lipnharski@escola.pr.gov.br>
2025-02-27 08:29:17 -0300

    Add files via upload

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,24 @@
+function somaNumeros(num1, num2) {
+    return num1 + num2;
+}
+console.log(somaNumeros(7, 9))
+
+function subtraiNumeros(num1, num2) {
+    return num1 - num2;
+}
+console.log(subtraiNumeros(7, 9))
+
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicacao(7, 9))
+
+function divisao(num1, num2) {
+    return num1 / num2;
+}
+console.log(divisao(7, 9))
+
+function restoDivisao(num1, num2) {
+    return num1 % num2
+}
+console.log(restoDivisao(10, 9))
