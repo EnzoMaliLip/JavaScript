7c171671357cb1ccd744bc305c61153de383730f
EnzoMaliLip <enzo.lipnharski@escola.pr.gov.br>
2025-05-13 08:53:42 -0300

    Add files via upload

---
+++ ATV 1 2°TRI/scr/Classes/Carro.php
@@ -0,0 +1,9 @@
+<?php
+
+class Carro
+{
+    public $nome;
+    public $marca;
+    public $descricao = "Carro estrangeiro";
+    public $preco;
+}
---
+++ ATV 1 2°TRI/scr/Classes/Vendedor.php
@@ -0,0 +1,9 @@
+<?php
+
+Class Vendedor
+{
+    public const PAIS = "Brasil";
+    public $razaoSocial;
+    public $nomeFantasia;
+    public $cnpj;
+}
